47421e3a48ca340eda5cce496a22b622435c11ff
Tatyana <tdrykova@edu.hse.ru>
2021-11-23 16:55:20 +0300

    second

--- src/main/java/com/database/Main.java
+++ src/main/java/com/database/Main.java
@@ -4,12 +4,33 @@ import java.sql.*;
 
 public class Main {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
-    private static final String USERNAME = "rroot";
-    private static final String PASSWORD = "rroot";
-
     public static void main(String[] args) {
-        Connection connection;
+
+        DBConnection connect = new DBConnection();
+
+      //  String query = "SELECT * FROM usersnew";
+        try {
+            Statement statement = connect.getConnection().createStatement();
+            //statement.executeQuery("SELECT * FROM usersnew");
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM usersnew");
+
+            while (resultSet.next()) {
+                User user = new User();
+                user.setId(resultSet.getInt("id"));
+                user.setUsername(resultSet.getString("username"));
+                user.setPassword(resultSet.getString("password"));
+                System.out.println(user);
+//                int id = resultSet.getInt(1);
+//                System.out.println(id); выводит колво запесей
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Connection is lost");
+        }
+
+
+        //lesson 5
+        //Connection connection;
 
         // try {
         // lesson 4
@@ -28,12 +49,37 @@ public class Main {
 //            System.out.println("Не удалось загрузить драйвер");
 //        }
 
-        try (Connection connection1 = DriverManager.getConnection(URL, USERNAME, PASSWORD); Statement statement = connection1.createStatement()) {
+       // try (Connection connection1 = DriverManager.getConnection(URL, USERNAME, PASSWORD); Statement statement = connection1.createStatement()) {
             // System.out.println(connection1.isClosed());
-            statement.execute("INSERT INTO users(name, age, email) VALUES ('name', 20, 'email')");
-        } catch (SQLException e) {
-            System.out.println("Request is failed");
-        }
+            //statement.execute("INSERT INTO users(name, age, email) VALUES ('name', 20, 'email')");
+            //-------------------------------------------
+//            statement.execute("INSERT INTO users(name, age, email) VALUES ('name', 20, 'email')");
+//            int res = statement.executeUpdate("UPDATE users SET name = 'Steve', age = 55 where id = 3");
+//            System.out.println(res);
+            //-------------------------------------------
+//            ResultSet res2 = statement.executeQuery("SELECT * FROM users");
+//            System.out.println(res2);
+            //-------------------------------------------
+//            statement.addBatch("INSERT INTO users(name, age, email) VALUES ('name', 21, 'email')");
+//            statement.addBatch("INSERT INTO users(name, age, email) VALUES ('name', 22, 'email')");
+//            statement.addBatch("INSERT INTO users(name, age, email) VALUES ('name', 23, 'email')");
+           // statement.executeBatch();
+// -----------------------------
+         //  statement.clearBatch();
+
+//            statement.addBatch("INSERT INTO users(name, age, email) VALUES ('name', 24, 'email')");
+//            statement.addBatch("INSERT INTO users(name, age, email) VALUES ('name', 25, 'email')");
+//            statement.addBatch("INSERT INTO users(name, age, email) VALUES ('name', 26, 'email')");
+//            statement.executeBatch();
+// -----------------------------
+           // System.out.println(statement.isClosed());
+// -----------------------------
+//            statement.getConnection();
+//           // statement.close();
+//
+//        } catch (SQLException e) {
+//            System.out.println("Request is failed");
+//        }
     }
 }
 
---
+++ src/main/java/com/database/Main2.java
@@ -0,0 +1,64 @@
+package com.database;
+
+import java.io.FileInputStream;
+import java.sql.*;
+import java.util.Calendar;
+
+public class Main2 {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/mydbtest";
+    private static final String USERNAME = "rroot";
+    private static final String PASSWORD = "rroot";
+
+    private static final String INSERT_NEW = "INSERT INTO users VALUES (?, ?, ?, ?)";
+    private static final String GET_ALL = "SELECT * FROM users";
+    private static final String DELETE = "DELETE FROM users WHERE id = ?";
+
+    public static void main(String[] args) {
+
+        Connection connection;
+        PreparedStatement preparedStatement = null;
+        PreparedStatement preparedStatement2 = null;
+        PreparedStatement preparedStatement3 = null;
+
+        try {
+
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            // ------------------------------------
+//            preparedStatement = connection.prepareStatement(INSERT_NEW);
+//            preparedStatement.setInt(1,1);
+//            preparedStatement.setString(2,"Name");
+//            preparedStatement.setInt(3, 33);
+//            preparedStatement.setString(4,"wfew");
+//            preparedStatement.execute();
+
+            //preparedStatement.setDate(4, new Date(Calendar.getInstance().getTimeInMillis()));
+           // preparedStatement.setBlob(4, new FileInputStream("image.png"));
+
+            //--------------------------------
+            // ПУНКТ 2
+            preparedStatement2 = connection.prepareStatement(GET_ALL);
+            ResultSet res2 = preparedStatement2.executeQuery();
+
+            while (res2.next()) {
+                int id = res2.getInt("id");
+                String name = res2.getString("name");
+                int age = res2.getInt("age");
+                String email = res2.getString("email");
+
+                System.out.println("id: " + id + ", name: " + name + "," +
+                        " age: " + age + ", email: " + email);
+            }
+            //----------------------------------------------------------
+            // УДАЛЕНИЕ 1 ой строки из ПУНКТА 2
+            preparedStatement3 = connection.prepareStatement(DELETE);
+            preparedStatement3.setInt(1, 1); // id: 1, name: Name, age: 33, email: wfew
+            preparedStatement3.executeUpdate();
+
+        } catch (SQLException e) {
+            System.out.println("Не удалось загрузить драйвер");
+        }
+
+    }
+}
+
